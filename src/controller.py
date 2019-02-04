1f730da3f3fe194193718fd7bc839279ddd60bb9
Celine <chuss@mifinity.com>
2019-02-04 21:16:38 +0000

    add xml method

--- src/controller.py
+++ src/controller.py
@@ -20,7 +20,7 @@ def postSomeThing():
     logger.info('trace: %s',traceid)
     ref=str(random.randrange(1,9))+"-"+str(random.randrange(1,9))+"-"+str(random.randrange(1111111,9999999))
     tmpresp = {'processorReference' : ref,
-      'secureTradingResultCode' : '0',
+      'ResultCode' : '0',
       'live' : 'false',
       'processorMoney': { 'amount': trxamount,
       'currency' : trxcur,
@@ -42,18 +42,14 @@ def getXLM():
     currency = content[1][2][0].attrib.get('currencycode')
 
     exp= ("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?><responseblock version=\"3.67\">"
-          "<requestreference>Wx-k4nfn1a0</requestreference>"
-          "<secrand>6iRxxxxiNwPkChI</secrand>"
-          "<response type=\"REFUND\">"
-          "<authcode>REFUND ACCEPTED</authcode>"
+          "<response type=\"RESPONSE\">"
+          "<code>ACCEPTED</code>"
           "<timestamp>"+currentDT.strftime("%Y-%m-%d %H:%M:%S")+"</timestamp>"
           "<live>1</live>"
-          "<transactionreference>"+ref+"</transactionreference>"
+          "<reference>"+ref+"</reference>"
           "<security><address>0</address><postcode>0</postcode><securitycode>XXX</securitycode></security>"
-          "<settlement><settleduedate>"+currentDT.strftime("%Y-%m-%d")+"</settleduedate><settlestatus>0</settlestatus></settlement>"
-          "<billing><payment type=\"MASTERCARDDEBIT\"><issuer>POSTEPAY S.P.A</issuer><issuercountry>IT</issuercountry><pan>XXXXXXXXXX</pan></payment>"
           "<amount currencycode="+currency+">"+amount+"</amount>"
-          "<dcc enabled=\"0\"/></billing><operation><accounttypedescription>ECOM</accounttypedescription></operation><error><message>Ok</message><code>0</code></error></response></responseblock>")
+          "<error><message>Ok</message><code>0</code></error></response></responseblock>")
 
 
     return exp
